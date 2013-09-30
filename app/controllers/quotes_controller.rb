bb5d65b1668e51cdbe21ca3da28bb31e5e977682
Katlyn Daniluk <pair+katlyn@mavenlink.com>
2013-09-30 09:13:30 -0700

    created first quotes controller using rulers controllers and routing

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,5 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "The least productive people are usually the ones who are most in favor of holding meetings."
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 # best_quotes/config/application.rb
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 require "rulers"
+require "quotes_controller"
 
 module BestQuotes
  class Application < Rulers::Application
